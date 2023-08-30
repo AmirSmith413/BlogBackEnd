ecf0dac088e321b18ec7427735f5fe21e7a66be4
AmirSmith413 <amirsmith166@gmail.com>
2023-08-29 19:41:08 -0700

    added tables

--- Services/Context/DataContext.cs
+++ Services/Context/DataContext.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using BlogBackEnd.Models;
 
 namespace BlogBackEnd.Services.Context
 {
     public class DataContext : DbContext
     {
+        public DbSet<UserModel> UserInfo {get; set;}
+        public DbSet<BlogItemsModels> BlogInfo {get; set;}
         public DataContext(DbContextOptions options) : base(options) {
-
+        }
+        //function to create the tables themselves
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
--- models/BlogItemsModels.cs
+++ models/BlogItemsModels.cs
@@ -5,8 +5,22 @@ using System.Threading.Tasks;
 
 namespace BlogBackEnd.Models
 {
-    public class BlogBackEnd
+    public class BlogItemsModels
     {
-        
+       public int Id {get;set;}
+       public int UserID {get;set;}
+
+       public string? PublisherName {get;set;}
+       public string? Title {get;set;}
+       public string? Tag {get;set;}
+       public string? Image {get;set;}
+       public string? Description {get;set;}
+       public string? Date {get;set;}
+       public string? Category {get;set;}
+
+       public bool IsPublished {get;set;}
+       public bool IsDeleted {get;set;}
+
+       public BlogItemsModels () {}
     }
 }
